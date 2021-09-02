81d78b8d95de95a0fe366f359462b704280e493d
Zixuan Cang <cangzx@gmail.com>
2021-09-02 09:39:50 -0400

    Modified __init__.py

--- scanit/__init__.py
+++ scanit/__init__.py
@@ -2,8 +2,7 @@
 import warnings
 warnings.filterwarnings('ignore')
 
-from . import preprocessing as pp
 from . import tools as tl
 
 import sys
-sys.modules.update({f'{__name__}.{m}': globals()[m] for m in ['pp','tl']})
+sys.modules.update({f'{__name__}.{m}': globals()[m] for m in ['tl']})
